b077a5a1701754e4174a3752ddfe5575cab00a8a
harinugraha129 <34996514+harinugraha129@users.noreply.github.com>
2019-12-09 01:38:30 +0700

    Add files via upload

---
+++ Luas Lingkaran/Luas Lingkaran/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Luas_Lingkaran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menghitung Luas Lingkaran");
+            Console.WriteLine();
+            Console.Write("masukan Jari-jari nya : ");
+            string jarix = Console.ReadLine();
+            int jari = Convert.ToInt32(jarix);
+            const float phi1=314;
+            const float phi2=3142857;
+            float phi;
+            if (jari % 7 == 0)
+                phi = phi2/1000000;
+            else
+                phi = phi1/100;
+
+            float hasil = jari * jari * phi;
+            float hasil2 = (jari + jari)* phi;
+            Console.WriteLine("Luas Lingkaran = "+ hasil );
+            Console.Write("Keliling Lingkaran = "+ hasil2 );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Luas Segitiga input mudah/ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Menghitung Luas Segitiga");
+            Console.Write("masukan Alas nya : ");
+            int alas = int.Parse(Console.ReadLine());
+            double koma;
+            koma = 0.5;
+            Console.Write("masukan tinggi nya : ");
+            int tinggi = int.Parse(Console.ReadLine());
+           
+            double hasil = alas * tinggi * koma;
+
+
+            Console.Write("Luas Segitiga adalah ");
+            Console.WriteLine(hasil);
+          
+
+            Console.ReadLine();
+        }
+    }
+}
